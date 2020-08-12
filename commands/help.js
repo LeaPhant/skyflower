4b5697abb07c52e08c630027c8144a636a26af19
LeaPhant <mail@lea.moe>
2020-08-12 15:45:35 +0200

    Fix help for !leaderboard command, use dynamic prefix

--- commands/help.js
+++ commands/help.js
@@ -7,17 +7,23 @@ module.exports = {
     call: obj => {
         const { argv } = obj;
 
-        return { embed: {
-            color: 11809405,
-            title: "Commands for Robot Lea",
-            description:
-`\`!bazaar [amount] <item>\` – Get bazaar prices for an item.
-\`!skills <user> [profile] [skill]\` – Get skill levels of a player.
-\`!leaderboard <leaderboard> [u:username] [skill]\` – Show leaderboards.
-\`!owoify [text]\` – owoify a text or the last message.
-\`!pack\` – Display info about the pack used on SkyLea.
-\`!rules [rule]\` – Show a specific server rule.`
-        }};
+        return { 
+            embed: {
+                color: 11809405,
+                title: "Commands for Robot Lea",
+                description:
+`\`${config.prefix}bazaar [amount] <item>\` – Get bazaar prices for an item.
+\`${config.prefix}skills <user> [profile] [skill]\` – Get skill levels of a player.
+\`${config.prefix}leaderboard <leaderboard> [u:username]\` – Show leaderboards.
+\`${config.prefix}owoify [text]\` – owoify a text or the last message.
+\`${config.prefix}pack\` – Display info about the pack used on SkyLea.
+\`${config.prefix}rules [rule]\` – Show a specific server rule.`,
+                footer: {
+                    icon_url: "https://cdn.discordapp.com/attachments/572429763700981780/726040184638144512/logo_round.png",
+                    text: `sky.lea.moe – <> required argument [] optional argument`
+                },
+            }
+        };
 
         //return helper.commandHelp(argv[1]);
     }
