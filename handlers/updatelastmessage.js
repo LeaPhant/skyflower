1a0146459a1dfe41ad3cc82ee9e14a9a1cad8faf
LeaPhant <mail@lea.moe>
2020-08-21 20:59:11 +0200

    Add support for updating last message in DM channel

--- handlers/updatelastmessage.js
+++ handlers/updatelastmessage.js
@@ -4,12 +4,14 @@ module.exports = {
     message: async obj => {
         let { client, msg, prefix, db } = obj;
 
+        const guildId = msg.guild != null ? msg.guild.id : 'me';
+
         if(msg.content.startsWith(prefix) 
         || msg.author.id == client.user.id 
         || msg.content == false)
             return;
         
-        db.set(`lm_${msg.guild.id}_${msg.channel.id}`, msg.content).catch(helper.error);
+        db.set(`lm_${guildId}_${msg.channel.id}`, msg.content).catch(helper.error);
     }
 };
  
