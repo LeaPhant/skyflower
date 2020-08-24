0825f630998ec1fabd8d461db38d09bd1fc046f6
LeaPhant <mail@lea.moe>
2020-08-24 22:39:32 +0200

    Add custom status

--- bot.js
+++ bot.js
@@ -16,6 +16,9 @@ const db = new Keyv(config.dbUri, { namespace: config.dbNamespace });
 const client = new Discord.Client();
 
 client.on('error', helper.error);
+client.on('ready', () => {
+	client.user.setActivity('s!help', { type: 'PLAYING', shardID: client.shard.ids })
+});
 
 async function main(){
 	const commands = [];
