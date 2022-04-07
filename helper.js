7d15570e24922b1727df460dc3eee8ef9b2a7d0d
LeaPhant <mail@lea.moe>
2022-04-08 00:37:20 +0200

    Add assert type to emote import

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ import distance from 'jaro-winkler';
 import fetch from 'node-fetch';
 
 import config from './config.json' assert { type: 'json' };
-import emotes from './emotes.json';
+import emotes from './emotes.json' assert { type: 'json' };
 
 const sep = ' ✦ ';
 const cmd_escape = "```";
